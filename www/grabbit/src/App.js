5b927dd4d8c1b20fd02fbf368011130993316f73
Rashad <ralston@Rashads-MBP.attlocal.net>
2021-01-03 14:36:47 -0800

    setup www with routes, components, templates, reducer

--- www/grabbit/src/App.js
+++ www/grabbit/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Router from 'Router';
+import Reducer from 'Reducer';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
 
 function App() {
+  const store = createStore(Reducer, applyMiddleware(thunk));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router />
+    </Provider>
   );
 }
 
